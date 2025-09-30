2a1b0122cb819a1c0f47e5b26e93956570be1f10
pengyu300 <pengyu.ye04@gmail.com>
2025-09-30 09:45:34 +0200

    Create programa02.py

---
+++ U1/programas_01/programa02.py
@@ -0,0 +1,13 @@
+"""
+programa02
+Escribe un programa que escriba por pantalla: el booleano True, dos literales enteros,
+dos literales flotantes y una cadena de caracteres, y para cada uno de ellos su tipo de
+datos.
+"""
+
+print("True:", True, "- Tipo:", type(True))
+print("Entero 1:", 10, "- Tipo:", type(10))
+print("Entero 2:", -5, "- Tipo:", type(-5))
+print("Flotante 1:", 9.14, "- Tipo:", type(9.14))
+print("Flotante 2:", -2.5, "- Tipo:", type(-2.5))
+print("Cadena:", "Hola Python", "- Tipo:", type("Hola Python"))
