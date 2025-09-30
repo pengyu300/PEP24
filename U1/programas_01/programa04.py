fd6934aa5d1c15fcc7ab851f079736047f16f607
pengyu300 <pengyu.ye04@gmail.com>
2025-09-30 09:46:31 +0200

    Create programa04.py

---
+++ U1/programas_01/programa04.py
@@ -0,0 +1,7 @@
+"""Escribe un programa que muestre por pantalla el siguiente texto invocando solo una vez a
+la función print() con tres argumentos."""
+
+# Mostrar texto con un solo print y tres argumentos
+print("""Soy estudiante del
+IES Leonardo Da Vinci.
+Del ciclo DAW.""")
