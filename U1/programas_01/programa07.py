42cc7d40daebab5280026c423036c34816dcd099
pengyu300 <pengyu.ye04@gmail.com>
2025-09-30 09:48:22 +0200

    Create programa07.py

---
+++ U1/programas_01/programa07.py
@@ -0,0 +1,18 @@
+"""
+Escribe un programa que use la función print() para
+ Escribir un el número 34 en binario, otro en octal y otro en hexadecimal.
+ Escribir los números 0b1001, 0o12, 0xa3f en decimal.
+"""
+
+n1 = 34
+n2 = 34
+n3 = 34
+
+print(bin(n1))   # binario
+print(oct(n2))   # octal
+print(hex(n3))   # hexadecimal
+
+# Números en decimal desde otras bases
+print(0b1001)  # binario a decimal
+print(0o12)    # octal a decimal
+print(0xa3f)   # hexadecimal a decimal
