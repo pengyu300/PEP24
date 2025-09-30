df2f3cc95ad839f5414f2f260a6c0f90ad8fd3da
pengyu300 <pengyu.ye04@gmail.com>
2025-09-30 09:46:04 +0200

    Create programa03.py

---
+++ U1/programas_01/programa03.py
@@ -0,0 +1,6 @@
+"""
+Escribe un programa que muestre por pantalla el siguiente texto invocando solo una vez a
+la función print() y con un solo argumento.
+"""
+
+print("Soy estudiante del\nIES Leonardo Da Vini.\nDel ciclo DAW.")
