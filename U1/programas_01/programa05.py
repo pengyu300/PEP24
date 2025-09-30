2cefdc9406466a7811fad805677bef6f3fb025ee
pengyu300 <pengyu.ye04@gmail.com>
2025-09-30 09:47:15 +0200

    Create programa05.py

---
+++ U1/programas_01/programa05.py
@@ -0,0 +1,10 @@
+"""En el siguiente programa:
+print("Fundamentos","Programación","en")
+print("Python")
+Modifica la primera línea de código, utilizando las palabras claves sep y end, para que
+coincida con el resultado esperado. No cambies nada en la segunda invocación de
+print().
+Fundamentos***Programación***en...Python"""
+
+print("Fundamentos", "Programacion", "en", sep="***", end="...")
+print("Python")
