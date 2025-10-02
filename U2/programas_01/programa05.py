25c990b24aa5849cbb4edcd863dda1559672db56
pengyu300 <pengyu.ye04@gmail.com>
2025-10-02 09:26:10 +0200

    Update programa05.py

--- U2/programas_01/programa05.py
+++ U2/programas_01/programa05.py
@@ -1,1 +1,12 @@
+# programa05
+# Escribe un programa que pida dos números y diga cuál es el menor, cuál el mayor o si son iguales.
 
+num1 = float(input("Introduce el primer numero: "))
+num2 = float(input("Introduce el segundo numero: "))
+
+if num1 > num2:
+    print(f"Mayor: {num1} Menor: {num2}")
+elif num2 > num1:
+    print(f"Mayor: {num2} Menor: {num1}")
+else:
+    print("Son iguales")
