14bae0ca14f1a4997a1576f39be2cd6ccc53b8cb
pengyu300 <pengyu.ye04@gmail.com>
2025-10-02 09:23:55 +0200

    Update programa02.py

--- U2/programas_01/programa02.py
+++ U2/programas_01/programa02.py
@@ -1,1 +1,15 @@
+# programa02
+# Escribe un programa que pida primero un número par (positivo o negativo). 
+# Si el valor no es correcto, muestra un aviso.
+# Si el valor es correcto, pide un número impar y si no es correcto muestra un aviso.
 
+par = int(input("Introduce un numero par: "))
+
+if par%2 != 0:
+    print("Debe ser un numero par")
+else:
+    impar = int(input("Introduce un numero impar: "))
+    if impar %2 == 0:
+        print("Debe ser un numero impar")
+    else:
+        print("Correcto")
