f2a4a6b310d70f981a68e4c944c4eec0a0d28f13
pengyu300 <pengyu.ye04@gmail.com>
2025-10-02 09:24:44 +0200

    Update programa03.py

--- U2/programas_01/programa03.py
+++ U2/programas_01/programa03.py
@@ -1,1 +1,12 @@
+# programa03
+# Escribe un programa que pida dos números y muestre su división.
+# No se puede dividir por 0 (mostrar aviso en ese caso).
 
+numero1 = float(input("Introduce el dividendo: "))
+numero2 = float(input("Introduce otro divisor: "))
+
+if numero2 == 0:
+    print("No se puede dividir por 0")
+else:
+    resultado = numero1 / numero2
+    print(f"Resultado:  {numero1} / {numero2} = {resultado}")
