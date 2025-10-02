c4a8dbb70b77e954591a1a8916442b0f41899cf9
pengyu300 <pengyu.ye04@gmail.com>
2025-10-02 09:27:54 +0200

    Update programa07.py

--- U2/programas_01/programa07.py
+++ U2/programas_01/programa07.py
@@ -1,1 +1,10 @@
+# programa07
+# Escriba un programa que pida un año y que escriba si es bisiesto o no. Un año es bisiesto
+# si es múltiplos de 4 pero no múltiplos de 100, aunque si los múltiplos de 400.
 
+anio = int(input("Introduce un anio: "))
+
+if (anio % 4 == 0 and anio % 100 !=0) or (anio % 400 == 0):
+    print(f"El año {anio} es bisiesto")
+else:
+    print(f"El año {anio} no es bisiesto")
