2217f24a2dbfd1127fbb29f9bd6002eed4a0da06
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 20:00:51 +0200

    Update programa01.py

--- U2/programas_02/programa01.py
+++ U2/programas_02/programa01.py
@@ -1,1 +1,16 @@
+"""
+programa01
+Escribe un programa que muestre una lista de números del 1 al 10. Resuelve el ejercicio
+de dos formas distintas, utilizando los bucles for y while. Cuando utilices el bucle for
+puedes hacer uso de la función range.
+"""
 
+print("Con bucle for: ")
+for i in range(1,11):
+    print(i, end= " ")
+
+print("\nCon bucle while: ")
+i = 1
+while i <= 10:
+    print(i, end=" ")
+    i += 1
