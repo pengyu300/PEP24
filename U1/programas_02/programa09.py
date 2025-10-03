ab7c3d89626d7361a3e7d0c3cf172a7f78a761c3
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:16:20 +0200

    Update programa09.py

--- U1/programas_02/programa09.py
+++ U1/programas_02/programa09.py
@@ -1,1 +1,11 @@
+# programa09
+# Escribe un programa que calcule la calificación de estudiante en un módulo. La calificación se obtiene de la calificación parcial en cada RA 
+# (RA1 20%, RA2, 60% y RA3 20%).
 
+RA1 = float(input("Introduce la calificacon de RA1: "))
+RA2 = float(input("Introduce la calificacon de RA2: "))
+RA3 = float(input("Introduce la calificacon de RA3: "))
+
+calificacion = RA1 * 0.2 + RA2 * 0.6 + RA3 * 0.2
+
+print(f"Nota final: {calificacion}")
