5c58f1f6c2fe3030178139524a39b7e376299e9e
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:16:03 +0200

    Update programa08.py

--- U1/programas_02/programa08.py
+++ U1/programas_02/programa08.py
@@ -1,1 +1,20 @@
+# programa08
+# Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente
+# desea saber cuanto deberá pagar finalmente por su compra.
+
+total = float(input("Introduce el total de la compra: "))
+descuento = total * 0.85
+
+print(f"Total con descuento: {descuento}")
+
+
+
+
+
+
+
+
+
+
+
 
