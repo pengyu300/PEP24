d4e89d3d64af92df18b9bb0c746c08fa8f293d45
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 20:01:57 +0200

    Update programa05.py

--- U2/programas_02/programa05.py
+++ U2/programas_02/programa05.py
@@ -1,1 +1,12 @@
+"""
+programa05
+Escribe un programa que pida un número y muestre una lista de números desde 1 al número. Se debe controlar que el número 
+no se menor que 1 ni mayor que 10, si es así se pedirá que si introduzca de nuevo, y así hasta que se introduzca el número correcto.
+"""
 
+num = 0
+while num < 1 or num > 10:
+    num = int(input("Introduce un numero entre 1 y 10: "))
+
+for i in range(1, num+1):
+    print(i, end=" ")
