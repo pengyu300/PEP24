0492aa77d18f47ff812487939e5ccbf88df6ea5a
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 20:01:05 +0200

    Update programa02.py

--- U2/programas_02/programa02.py
+++ U2/programas_02/programa02.py
@@ -1,1 +1,9 @@
+# programa02
+# Escribe un que lea por teclado un número comprendido entre 1 y 10. No se dejara de
+# pedir el número hasta que no se introduzca correctamente.
 
+numero = 0
+while numero < 1 or numero > 10:
+    numero= int(input("Introduce un numero entre 1 y 10: " ))
+
+print(f"Numero valido: {numero}")
