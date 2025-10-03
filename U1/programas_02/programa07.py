4a8558b2f244d1b26946b7a85175997b5660e1ce
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:15:42 +0200

    Update programa07.py

--- U1/programas_02/programa07.py
+++ U1/programas_02/programa07.py
@@ -1,1 +1,11 @@
+# programa07
+# Escribe un programa que reciba una cantidad de minutos y muestre por pantalla a
+# cuantas horas y minutos corresponde.
+
+min = int(input("Introduce una cantidad de minutos: "))
+
+horas = min // 60
+minutos = min % 60
+
+print(f"{min} minutos son {horas} horas y {minutos} minutos")
 
