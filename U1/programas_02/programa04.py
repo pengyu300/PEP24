c1edfbc94d793d4c3acee5c922ee77ce7ab14811
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:14:38 +0200

    Update programa04.py

--- U1/programas_02/programa04.py
+++ U1/programas_02/programa04.py
@@ -1,1 +1,11 @@
+# programa04
+# Escribe un programa que pregunte la base y altura de una rectángulo y calcule su área y perímetro.
 
+base = float(input("Introduce la base del rectángulo: "))
+altura = float(input("Introduce la altura del rectángulo: "))
+
+area = base * altura
+perimetro = 2 * (base + altura)
+
+print("Área:", area)
+print("Perímetro:", perimetro)
