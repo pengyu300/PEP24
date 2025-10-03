1a07623782d7b68cb999d7ae10bd8127436c2129
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:14:02 +0200

    Update programa03.py

--- U1/programas_02/programa03.py
+++ U1/programas_02/programa03.py
@@ -1,1 +1,6 @@
+# programa03
+# Escribe un programa que pregunte al usuario su nombre y edad y lo muestre por pantalla.
 
+nombre = input("Introduce tu nombre: ")
+edad = input ("Introduce tu edad: ")
+print("Nombre:", nombre, " Edad:", edad)
