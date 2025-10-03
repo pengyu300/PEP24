369705fbf3135bdb8f886542c56fd04185ea6f2e
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:15:20 +0200

    Update programa06.py

--- U1/programas_02/programa06.py
+++ U1/programas_02/programa06.py
@@ -1,1 +1,7 @@
+# programa06
+# Escribe un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
 
+Fahrenheit = float(input("Introduce temperatura en Fahrenheit: "))
+Celsius = (Fahrenheit - 32) * 5/9
+
+print("Grados Fahrenheit a grados Celsius:", round(Celsius,2))
