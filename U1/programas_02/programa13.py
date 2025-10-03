77d52e7210046a6871fb4b3a8b8041a051a3caba
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:17:37 +0200

    Update programa13.py

--- U1/programas_02/programa13.py
+++ U1/programas_02/programa13.py
@@ -1,1 +1,9 @@
+"""
+programa13
+Usando uno de los operadores de comparación en Python, escribe un programa simple de
+dos líneas que tome el parámetro n como entrada, que es un entero, e imprima False si n
+es menor que 100, y True si n es mayor o igual que 100. No debes crear ningún bloque if.
+"""
 
+num = int(input("Introduce un numero: "))
+print(num >= 100)
