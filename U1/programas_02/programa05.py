afb1e83b9148082b5b3ec1421ffcbafb662d6ccf
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:14:52 +0200

    Update programa05.py

--- U1/programas_02/programa05.py
+++ U1/programas_02/programa05.py
@@ -1,1 +1,13 @@
+# programa05
+# Escribe un programa que pregunte al usuario dos números y calcule su suma, resta,
+# multiplicación, división, módulo y potencia.
 
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicacion:", num1*num2)
+print("Division:", num1/num2)
+print("Modulo:", num1 % num2)
+print("Potencia:", num1 ** num2)
