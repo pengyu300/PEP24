326740353d62dc1c9d9401c0f78a10a3ee4193e4
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:16:38 +0200

    Update programa10.py

--- U1/programas_02/programa10.py
+++ U1/programas_02/programa10.py
@@ -1,1 +1,9 @@
+# programa10
+# Escribe un programa que dado un número de dos cifras, diseñe un algoritmo que permita obtener el número invertido.
 
+num = int(input("Introduce un numero de dos cifras: "))
+decenas = num // 10
+unidades = num % 10
+invertido = unidades * 10 + decenas
+
+print("Número invertido:", invertido)
