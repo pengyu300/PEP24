681a36d5808b3def7410172acb63c9f3aed3ce24
pengyu300 <pengyu.ye04@gmail.com>
2025-10-03 18:08:56 +0200

    Update programa01.py

--- U1/programas_02/programa01.py
+++ U1/programas_02/programa01.py
@@ -1,1 +1,16 @@
+"""
+programa01
+Escribe un programa que cree cuatro variables que almacenen respectivamente un
+entero, un float, un booleano y una cadena de caracteres. A continuación que muestre por
+pantalla en valor de cada una de ellas y el tipo de datos de los objetos a los que apuntan
+"""
 
+entero = 3
+float = 2,22
+booleano = True
+cadena = "Me llamo Pengyu"
+
+print (entero, type(entero))
+print (float, type(float))
+print (booleano, type(booleano))
+print (cadena, type(cadena))
