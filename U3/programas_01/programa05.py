ada8347d44f65d97449ee041e4ad2ded635706ed
pengyu300 <pengyu.ye04@gmail.com>
2025-10-08 12:09:49 +0200

    Update programa05.py

--- U3/programas_01/programa05.py
+++ U3/programas_01/programa05.py
@@ -1,1 +1,54 @@
+"""
+programa05
+Escribe un programa donde crees varias funciones y pruebes el ámbito de las variables
+en Python (globales, no locales y locales).
+"""
 
+# Variables locales
+def ejemplo_local1():
+    n = 2
+    print("Dentro de la función:" ,n)
+
+ejemplo_local1()
+
+
+def ejemplo_local2():
+    n = 2
+    print("Dentro de la funcion:" ,n)
+    return
+
+n = 5
+ejemplo_local2()
+print("Fuera de la funcion:" ,n)
+
+
+# Variables globales
+a = 10
+
+def ejemplo_global1():
+    print("Variable global dentro de la funcion: ",a)
+
+ejemplo_global1()
+print("Variable global fuera de la funcion: ",a)
+
+
+def ejmplo_global2():
+    print("Dentro de la funcion:" ,b)
+
+b = 5
+ejmplo_global2()
+print("Fuera de la funcion: ",b)
+
+
+# Variables no locales
+def ejemplo_noLocal():
+    def sub_noLocal():
+        print("Desde la funcion interna: ",c)
+    
+    c = 3
+    sub_noLocal()
+    print("Desde la funcion externa: ",c)
+
+c = 4
+ejemplo_noLocal()
+print("Variable global: ",c)
