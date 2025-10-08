e872ad6cf60d0d41d90968dba7212376e7634fab
pengyu300 <pengyu.ye04@gmail.com>
2025-10-08 12:06:56 +0200

    Update programa01.py

--- U3/programas_01/programa01.py
+++ U3/programas_01/programa01.py
@@ -1,1 +1,13 @@
+"""
+programa01
+Escribe un programa en Python que haga uso de una función llamada saludar que cumpla los siguientes requisitos:
+    • Entrada: Tiene 3 parámetros de entrada, que serán 3 cadenas de texto: nombre,
+    primer apellido, segundo apellido
+    • Salida: No tiene parámetros de salida.
+    • Funcionalidad: Imprime por pantalla un mensaje saludando a la persona en función de los parámetros de entrada.
+"""
 
+def saludar(nombre, apellido1, apellido2):
+    print(f"Hola {nombre} {apellido1} {apellido2}")
+
+saludar("Pengyu", "Ye", "Ye")
